9dadd7a1a5e5011ad2030694fd2d75b33551e886
christianm20358 <christianm20358@gmail.com>
2022-07-01 18:53:14 -0600

    Created cmd package

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,2 +1,10 @@
 package cmd
 
+import (
+	//"github.com/ganymede-security/san/server"
+	//"github.com/ganymede-security/san/engine"
+	//"github.com/ganymede-security/san/router"
+
+)
+
+// Function to initialize a San server
