1ef5a5a88bcdd2d74d1e190505a1b656738cdf28
christianm20358 <christianm20358@gmail.com>
2022-07-07 09:27:38 -0600

    Refactor branch

--- server/middleware/baselines/baselines.go
+++
@@ -1,7 +0,0 @@
-// Package baselines is for tracking and measuring server baselines
-// for security and for measuring the health of the server over time.
-package baselines
-
-type Handler struct {
-	
-}
--- server/router/router.go
+++
@@ -1,9 +0,0 @@
-package router
-
-import "net/http"
-
-// Wrapper around a ServeMux
-type Router struct {
-	serveMux http.ServeMux
-}
-
