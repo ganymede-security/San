dfd4c8bf6b1bacac8391fe03bafe2a7e5a94fb8a
christianm20358 <christianm20358@gmail.com>
2022-07-08 14:32:04 -0600

    demux initial commit

--- DeMux/demux.go
+++ DeMux/demux.go
@@ -1,2 +1,37 @@
 package demux
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"syscall"
+)
+
+type Driver struct {
+	ListenPort	string
+	ReadBuffer string
+	WriteBuffer string
+}
+
+// Constants used by Driver when none are specified
+const (
+	DefaultWriteBuffer = 4096
+	DefaultReadBuffer = 4096
+)
+
+var (
+	addr = flag.String("s", "localhost", "server address")
+	port = flag.Int("p", 8972, "port")
+)
+
+func Listen() {
+
+	conn, err := net.ListenPacket("udp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cc := conn.(*net.IPConn)
+	cc.SetReadBuffer(20 )
+
+}
